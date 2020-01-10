e5738fafccf579cd1390d7e82e2394a30c727b0f
Felipe Lozada <felipe@penca.com.mx>
2020-01-10 13:46:49 -0600

    La aogiuna ya es en su mayoria responsive, tiene algunas media queries para esto, los link de redes sociales funcionan y la parte del correo sigue en proceso. A este punto los datos enviados por el formluario ya son validados

---
+++ index.view.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="assets/ico/cc_faviconSmaller.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+
+    <title>Construyendo Ciudadanos</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <header class="row">
+            <div class="overlay col-12">
+                <!--Menu-->
+                <div class="container-fluid">
+                    <nav class="header__navWrapper navbar navbar-expand-lg">
+                        <picture class="header__navLogo navbar-navbar-brand">
+                            <img src="assets/ico/cc_logo.png" alt="Logo de Construyendo Ciudadanos" class="header__navLogo--img">
+                        </picture>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"><img src="assets/ico/cc_menu.png" class="img-fluid pull-xs-left" alt="Menu"></span>
+                      </button>
+                        <div class="collapse navbar-collapse" id="navbarResponsive">
+                            <ul class="header__nav navbar-nav ml-auto">
+                                <li class="header__nav--item nav-item">
+                                    <a href="#somos">¿Qu&iacute;enes somos?</a>
+                                </li>
+                                <li class="header__nav--item nav-item">
+                                    <a href="#ejes">Nuestros ejes</a>
+                                </li>
+                                <li class="header__nav--item nav-item">
+                                    <a href="#registro">&Uacute;nete al cambio</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+                <!--Video-->
+                <video autoplay loop muted class="header__videoBackground">
+                  <source src="assets/vid/cc_video.mp4" type="video/mp4">
+              </video>
+                <!--Texto-->
+                <div class="row w-100 h-100 header__text">
+                    <h2 class="header__textTyped">Construyendo: Respeto, Uni&oacute;n, Ciudadanos</h2>
+                    <p class="header__textNormal">
+                        Somos una organizaci&oacute;n que busca cambiar su comunidad por medio de acciones
+                    </p>
+                    <img src="assets/ico/cc_tecolote.png" alt="Tecolote" class="tecolote ">
+                </div>
+            </div>
+        </header>
+        <div class="container-fluid">
+            <section class="row m-100 h-100" id="somos">
+                <div class="col col-12 ">
+                    <h2 class="text-center">¿Quienes Somos?</h2>
+                    <p class="text-justify">
+                        <h3 class="text-center"> Somos Contruyendo Ciudadanos:</h3>
+                        <br> Una organización que busca el cambiar su comunidad por medio de acciones, comenzando desde la limpieza, respeto, esfuerzo y la unión ciudadana. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus maiores amet
+                        eum, rem quis corporis non dolore consectetur vero quibusdam, beatae omnis in recusandae laboriosam cumque nostrum! Consectetur quidem, numquam!
+                    </p>
+                </div>
+
+                <div class="col col-12 col-md-12 col-lg-6">
+                    <h2>Nuestra Misión</h2>
+                    <img src="assets/img/cc_mision.jpeg" class="img-fluid" alt="Mision de Construyendo Ciudadanos" width="500px" height="300px">
+                    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="col col-12 col-md-12 col-lg-6">
+                    <h2>Nuestra Visión</h2>
+                    <img src="assets/img/cc_vision.jpeg" class="img-fluid" alt="Vision de Construyendo Ciudadanos" width="500px" height="300px">
+                    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </section>
+            <section class="row justify-content-around align-items-center" id="ejes">
+                <div class="container">
+                <div class="col col-12 align-items-center">
+                    <h1>Nuestros Ejes</h1>
+                </div>
+                <div class="col col-12 col-md ">
+                    <h3>Economia y trabajo.</h3>
+                </div>
+                <div class="col col-12 col-md ">
+                    <h3>Educacion y cultura</h3>
+                </div>
+                <div class="col col-12 col-md ">
+                    <h3>Salud y medio ambiente</h3>
+                </div>
+                <div class="col col-12 col-md">
+                    <h3>Transparencia</h3>
+                </div>                    
+                </div>
+            </section>
+            <section class="row" id="registro">
+                <div class="col-12">
+                    <h1>&Uacute;nete al cambio</h1>
+                </div>
+                <div class="col col-12 col-md">
+                    <h2>Direccion</h2>
+                    <p class="text-justify">
+                        Recuerda que nos reunimos los lunes entre la Av. Domingo Muños y Av. Ingnacio Allende. <br> A lado de la farmacia naturista y el caf&eacute; "Kronos" en Ciudad Sahag&uacute;n a las 07:00pm. Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit. Repellat expedita quidem magni architecto tempora maxime, soluta delectus repellendus. Eligendi ducimus voluptas enim quae facilis tenetur aut nobis tempora perferendis repellat. Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit. Praesentium suscipit vel voluptatem veniam non.
+                    </p>
+                </div>
+                <div class="col col-12 col-md">
+                    <h2>Dejanos tus datos y nos pondremos en contacto contigo</h2>
+                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST" class="form-horizontal">
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="nombre">Nombre: </label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Escribe tu nombre" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="email">Correo: </label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Escribe tu correo" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="region" class="control-label col-sm-2">Comunidad: </label>
+                            <div class="col-sm-10">
+                                <select name="region" id="region" name="region" class="form-control">
+                                <option>Elije la comunidad a la que perteneces:</option>
+                                <option value="Tepeapulco">Tepeapulco</option>
+                                <option value="Irolo">Irolo</option>
+                                <option value="Tepetates">Tepetates</option>
+                                <option value="Ciudad Sahagún">Ciudad Sahag&uacute;n</option>
+                              </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="coments">Comentarios: </label>
+                            <div class="col-sm-10">
+                                <textarea class="form-control" name="coments" id="coments" rows="10" required></textarea>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-10">
+                                <button type="submit" class="btn btn-primary btn-block">Enviar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        </div>
+        <div class="container-fluid">
+        <footer class="row align-content-between h-100 align-items-center">
+        <div class="col col-12 col-lg-4">© 2020 Contruyendo Ciudadanos.</div>
+            <div class="col col-12 col-lg-4"><a class="footer__link" href="#">Terminos y condiciones</a></div>
+            <div class="col col-12 col-lg-4">Siguenos en nuestras redes sociales
+                <div class="row align-content-lg-center">
+                    <div class="col col-12 col-lg-6"><a class="footer__link" href="https://facebook.com/Construyendo-Ciudadanos-112941933444981/" target="_blank">Facebook: <img src="assets/ico/cc_facebook.png" alt="Redes Sociales de Construyendo ciudadanos: Facebook" width ="40px" heigth = "40px"></a></div>
+                    <div class="col col-12 col-lg-6"><a class="footer__link" href="#
+                        instagram.com/construyendociudadanos/" target="_blank">Instagram: <img src="assets/ico/cc_instagram.png" alt="Redes Sociales de Construyendo ciudadanos: Instagram" width ="40px" heigth = "40px"></a></div>
+                </div>
+            </div>
+        </footer>
+        </div>
+
+    </div>
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
