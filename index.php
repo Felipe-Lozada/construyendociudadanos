d1f329f81f42715c7b4e04443d75b40ab98307ff
Felipe Lozada <felipe@penca.com.mx>
2020-01-10 17:26:59 -0600

    Ajustes en el tanaño de fuente para pantallas pequeñas

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
     require 'index.view.php';
+    require 'libs/PHPMailer.php';
+    require 'libs/SMTP.php';
     // Obteniendo datos del usuario
     $errors = '';
     if ($_SERVER['REQUEST_METHOD'] == 'POST') 
@@ -17,5 +19,34 @@
 
         //enviando el email
         $contact = 'contacto@construyendociudadanos.org';
+
+        $mail = new PHPMailer(true);
+        try
+        {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.ionos.mx';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'contacto@construyendociudadanos.org';                     // SMTP username
+            $mail->Password   = '';                               // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+            $mail->Port       = 587;                                    // TCP port to connect to
+            
+            //Recipients
+            $mail->setFrom($user_mail, $user_name);
+
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            $mail->send();
+            echo 'Message has been sent';
+        }
+        catch (Exception $e) 
+        {
+         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
     }
 ?>
